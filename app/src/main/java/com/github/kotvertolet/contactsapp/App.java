0aa7ad79311f80f5d63a472a8cd86fc499f071fd
kotvertolet <bytesurfer5005@gmail.com>
2018-12-18 16:58:22 +0200

    base url extracted into a separate variable

--- app/src/main/java/com/github/kotvertolet/contactsapp/App.java
+++ app/src/main/java/com/github/kotvertolet/contactsapp/App.java
@@ -4,26 +4,27 @@ import android.app.Application;
 
 import com.github.kotvertolet.contactsapp.networking.WowApi;
 
+import lombok.Getter;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class App extends Application {
 
+    private static final String BASE_URL = "https://file.wowapp.me";
     private static WowApi wowApi;
-    private Retrofit retrofit;
-
-    public static WowApi getWowApi() {
-        return wowApi;
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://file.wowapp.me")
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
                 .addConverterFactory(JacksonConverterFactory.create())
                 .build();
         wowApi = retrofit.create(WowApi.class);
+    }
+
+    public static WowApi getWowApi() {
+        return wowApi;
     }
 }
